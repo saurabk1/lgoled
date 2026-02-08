8c92810290f08065110c4095830ae479c1884349
Saurabh Kumar <saurabk1@uci.edu>
2026-02-07 21:41:26 -0800

    Initial Commit

---
+++ LGOLEDRemote/LGOLEDRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LGOLEDRemoteApp.swift
+//  LGOLEDRemote
+//
+//  Created by SK on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct LGOLEDRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LGOLEDRemoteTests/LGOLEDRemoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  LGOLEDRemoteTests.swift
+//  LGOLEDRemoteTests
+//
+//  Created by SK on 2/7/26.
+//
+
+import Testing
+@testable import LGOLEDRemote
+
+struct LGOLEDRemoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
